e9add5cfd385420d969e2c6cb3fe3582006fadc6
bethan pickering <bethan.walton1@gmail.com>
2024-11-20 11:58:59 +0000

    made changes to yaml

--- db/CreateTable.sql
+++ db/CreateTable.sql
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS users
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Ben','Hesketh','test@test7.com'),(2,'Luke','Benson','test@test.com'),(3,'Matt','Hunt','test4@test.com');
+INSERT INTO `users` VALUES (1,'Ben','Hesketh','test@test7.com'),(2,'Luke','Benson','test@test.com'),(3,'Matt','Hunt','test4@test.com'),(4,'Barnaby','Riecher','test36@test.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
